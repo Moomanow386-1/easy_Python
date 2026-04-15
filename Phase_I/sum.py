75873a6fb11d8ff99c6951b9bdc72279af9d7947
Moomanow386-1 <nutthiwut.s@kkumail.com>
2026-04-16 04:25:23 +0700

    add loop

--- Phase_I/sum.py
+++ Phase_I/sum.py
@@ -1,5 +1,19 @@
-sum = 0
-for i in range(1, 6):
-    sum += i
-    if i == 5:
-        print("ผลรวมของตัวเลขคือ : ", sum)
+# total = 0
+# for i in range(1, 6):
+#     total += i
+# print("ผลรวมของตัวเลขคือ : ", total)
+
+# total = 0
+# for i in range(1, 6):
+#     number = int(input("ป้อนตัวเลขที่ " + str(i) + " : ")) 
+#     total += number       
+# print("ผลรวมของตัวเลขคือ : ", total)
+
+total = 0
+while True:
+    number = int(input("ป้อนตัวเลข: "))
+    if number <= 0: #ถ้าป้อนเลข 0 จะหยุดการทำงานของ loop
+        break
+    total += number
+print("ผลรวมของตัวเลขคือ : ", total)
+        
